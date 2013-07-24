36c572b9e5cd3ea0139a6a63adcfc04542b4df16
johanaberg <abergster@gmail.com>
2013-07-24 22:59:47 +0200

    add mov file, basic tractor cut demo

--- moved/base/sequence.py
+++ moved/base/sequence.py
@@ -10,8 +10,8 @@ class Sequence(object):
 
     def create_tractor(self):
         tractor = mlt.Tractor()
-        tractor.mark_in = 0
-        tractor.mark_out = 100
+        # tractor.mark_in = 0
+        # tractor.mark_out = 22
         return tractor
 
     def connect_playlist_to_multi_track(self, multi_track, playlist):
@@ -28,15 +28,22 @@ class Sequence(object):
 
     def create_producer(self, file_path):
         producer = mlt.Producer(self.profile, file_path)
-        producer.id = 123
-        # producer.clip_in = -1 # inclusive. -1 == not set
-        # producer.clip_out = -1 # inclusive, -1 == not set
+        producer.id = file_path
+        # producer.set_in_and_out(11, 22)
+        # producer.clip_in = 1 # inclusive. -1 == not set
+        # producer.clip_out = 11 # inclusive, -1 == not set
+        print 'fps', producer.get_fps()
         return producer
 
 
 if __name__ == "__main__":
 
+    """
+    basic usage of a mlt tractor
+    """
+
     file_name = '/home/aberg/PycharmProjects/MovEd/res/mov/wheel.mov'
+    file_name2 = '/home/aberg/PycharmProjects/MovEd/res/mov/wheel2.mov'
 
     # m = Mlt()
     seq = Sequence()
@@ -44,9 +51,12 @@ if __name__ == "__main__":
     tractor = seq.create_tractor()
     multi_track = seq.create_multi_track(tractor)
     playlist = seq.create_playlist()
+
     producer = seq.create_producer(file_name)
-    playlist.append(producer)
-    # playlist.connect_producer(producer)
+    producer2 = seq.create_producer(file_name2)
+    playlist.append(producer, 44, 88)
+    playlist.append(producer2, 22, 66)
+
     seq.connect_playlist_to_multi_track(multi_track, playlist)
 
     consumer = mlt.Consumer()
@@ -54,9 +64,9 @@ if __name__ == "__main__":
     consumer.set("real_time", 1)
     consumer.connect(tractor)
 
-    producer.seek(1)
-    producer.set_speed(1)
+    # producer.seek(1)
+    # producer.set_speed(1)
     consumer.start()
 
     while not consumer.is_stopped():
-        sleep(.22)
+        sleep(.25)
